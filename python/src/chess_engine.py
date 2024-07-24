1d74e0964da8e73c98a1a8a911f64e0eb14712e0
KicvazB <virgile.baisnee@live.com>
2024-07-24 19:33:38 +0200

    Added king and knight moves

--- python/src/chess_engine.py
+++ python/src/chess_engine.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .move_generator import generate_rook_moves, generate_bishop_moves, generate_queen_moves # generate_knight_moves, generate_king_moves, generate_pawn_moves
+from .move_generator import generate_rook_moves, generate_bishop_moves, generate_queen_moves, generate_king_moves, generate_knight_moves # generate_pawn_moves
 
 class Board():
     
@@ -83,5 +83,15 @@ class Board():
     
     def get_queen_moves(self, position):
         all_moves = generate_queen_moves(position)
+        #Check valid moves
+        return all_moves
+    
+    def get_king_moves(self, position):
+        all_moves = generate_king_moves(position)
+        #Check valid moves
+        return all_moves
+    
+    def get_knight_moves(self, position):
+        all_moves= generate_knight_moves(position)
         #Check valid moves
         return all_moves
--- python/src/move_generator.py
+++ python/src/move_generator.py
@@ -59,3 +59,61 @@ def generate_queen_moves(position):
     bishop_moves = generate_bishop_moves(position)
     moves = np.union1d(rook_moves, bishop_moves)
     return moves 
+
+def generate_king_moves(position):
+    directions = [8, 7, 9, 1, -1, -8, -7, -9]  # Up, UpLeft, UpRight, Right, Left, Down, DownRight, DownLeft
+    moves = []
+    for direction in directions:
+
+        new_position = position + direction
+
+        # Check if new_position is out of bounds
+        if new_position < 0 or new_position >= 64:
+            break
+
+        # Boundary checks to prevent wrapping around the edges
+        new_row = new_position // 8
+        new_col = new_position % 8
+
+        # Check if the movement is invalid for horizontal directions
+        if direction == 1 and new_position % 8 == 0:  # Moving right
+            break
+        if direction == -1 and new_position % 8 == 7:  # Moving left
+            break
+
+
+        # Moving up-left or down-right
+        if direction in [7, -9]:
+            if new_col > position % 8 or abs(new_row - position // 8) != abs(new_col - position % 8):
+                break
+
+        # Moving up-right or down-left
+        if direction in [9, -7]:
+            if new_col < position % 8 or abs(new_row - position // 8) != abs(new_col - position % 8):
+                break
+
+
+        moves.append(new_position)
+    return moves
+
+def generate_knight_moves(position):
+    moves = []
+    directions = [6, 15, 17, 10, -6, -15, -17, -10]  # NoWeWe, NoNoWe, NoNoEa, NoEaEa, SoEeEa, SoSoEa, SoSoWe, SoweWe
+    
+    for direction in directions:
+        new_position = position + direction
+
+        # Check if new_position is out of bounds
+        if new_position < 0 or new_position >= 64:
+            continue  # Continue instead of break to check other directions
+
+        # Boundary checks to prevent wrapping around the edges
+        current_row, current_col = divmod(position, 8)
+        new_row, new_col = divmod(new_position, 8)
+
+        if abs(current_row - new_row) == 2 and abs(current_col - new_col) == 1:
+            moves.append(new_position)
+        elif abs(current_row - new_row) == 1 and abs(current_col - new_col) == 2:
+            moves.append(new_position)
+
+    return moves
