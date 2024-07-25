e74d3c01a40131d7d4f5727f659320f617e01607
KicvazB <virgile.baisnee@live.com>
2024-07-25 19:09:32 +0200

    Simplified utils test

--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -1,18 +1,15 @@
 import pytest
 import numpy as np
 
-from src.board import Board
 from src.utils import index_to_chess_notation, chess_notation_to_index
 
 def test_conversion_index_to_chess_notation():
-    board = Board()
     index = 0 
     result = index_to_chess_notation(index)
     expected_chess_notation = 'a1'
     assert result == expected_chess_notation
 
 def test_conversion_chess_notation_to_index():
-    board = Board()
     chess_notation = 'h8'
     result = chess_notation_to_index(chess_notation)
     expected_index = 63
