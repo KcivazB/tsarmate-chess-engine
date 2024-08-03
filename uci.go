2c75953fd473920c76439809b7016bd68e251268
KicvazB <virgile.baisnee@live.com>
2024-08-03 23:06:23 +0200

    Implemented more uci commands

--- uci.go
+++ uci.go
@@ -12,38 +12,122 @@ var tell = mainTell
 var trim = strings.TrimSpace
 var low = strings.ToLower
 
-func uci(frGUI chan string, myTell func(text ...string)) {
+var bestMove = ""
+
+func uci(input chan string) {
 	tell("info string Hello from uci")
-	fromEng, toEng := engine()
+	toEng, fromEng := engine()
+	bInfinite := false
+
+	var cmd string
+	var bm string
+
 	quit := false
-	cmd := ""
-	words := []string{}
-	bm := ""
 	for !quit {
 		select {
-		case cmd = <-frGUI:
-			words = strings.Split(cmd, " ") // Get the words from the GUI
+		case cmd = <-input:
 		case bm = <-fromEng:
 			// Get the best move from the engine
-			handleBestMove(bm) //Handle the best move
+			handleBestMove(bm, bInfinite) //Handle the best move
 			continue
 		}
-
+		words := strings.Split(cmd, " ") // Get the words from the GUI
 		words[0] = trim(low(words[0]))
 		switch words[0] { // Depending on the first word
 		case "uci":
 			handleUCI()
-		case "isready":
-			handleIsReady()
 		case "setoption":
 			handleSetOption(cmd)
+		case "isready":
+			handleIsReady()
+		case "ucinewgame":
+			handleNewGame()
+		case "position":
+			handlePosition(words)
+		case "debug":
+			handleDebug(words)
+		case "register":
+			handleRegister(words)
+		case "go":
+			handleGo(words)
+		case "ponderhit":
+			handlePonderhit()
 		case "stop":
-			handleStop(toEng)
+			handleStop(toEng, &bInfinite)
 		case "quit", "q":
 			quit = true
 			continue
 		}
 	}
+}
+
+func handlePonderhit() {
+	tell("info string ponderhit not implemented yet")
+}
+
+func handleDebug(words []string) {
+	tell("info string debug not implemented yet")
+}
+
+func handleGo(words []string) {
+	if len(words) > 1 {
+		words[1] = trim(low(words[1]))
+		switch words[1] {
+		case "searchmoves":
+			tell("info string go searchmoves not implemented yet")
+		case "ponder":
+			tell("info string go ponder not implemented yet")
+		case "wtime":
+			tell("info string go wtime not implemented yet")
+		case "btime":
+			tell("info string go btime not implemented yet")
+		case "winc":
+			tell("info string go winc not implemented yet")
+		case "binc":
+			tell("info string go binc not implemented yet")
+		case "movestogo":
+			tell("info string go movestogo not implemented yet")
+		case "depth":
+			tell("info string go depth not implemented yet")
+		case "nodes":
+			tell("info string go nodes not implemented yet")
+		case "movetime":
+			tell("info string go movetime not implemented yet")
+		case "mate":
+			tell("info string go mate not implemented yet")
+		case "infinite":
+			tell("info string go infinite not implemented yet")
+
+		default:
+			tell("info string go ", words[1], " not implemented yet")
+		}
+	} else {
+		tell("info string go not implemented yet")
+	}
+
+}
+
+func handleRegister(words []string) {
+	tell("info string register not implemented yet")
+}
+
+func handlePosition(words []string) {
+	if len(words) > 1 {
+		words[1] = trim(low(words[1]))
+		switch words[1] {
+		case "startpos":
+			tell("info string position startpos not implemented yet")
+		case "fen":
+			tell("info string position fen not implemented yet")
+		default:
+			tell("info string position ", words[1], " not implemented yet")
+		}
+	}
+
+}
+
+func handleNewGame() {
+	tell("info string ucinewgame not implemented yet")
 }
 
 func handleUCI() {
@@ -65,12 +149,22 @@ func handleSetOption(option string) {
 	tell("info string not implemented yet")
 }
 
-func handleBestMove(bm string) {
-	tell(bm)
+func handleBestMove(bm string, bInfinite bool) {
+	if bInfinite {
+		bestMove = bm
+	}
+	tell(bestMove)
 }
 
-func handleStop(toEng chan string) {
-	toEng <- "stop"
+func handleStop(toEng chan string, bInfinite *bool) {
+	if *bInfinite {
+		if bestMove != "" { // If no best move yet
+			tell(bestMove) // tell the GUI we have a best move
+			bestMove = ""  // And we set it back to ""
+		}
+		toEng <- "stop" // Stop the engine function
+		*bInfinite = false
+	}
 }
 
 func input() chan string {
